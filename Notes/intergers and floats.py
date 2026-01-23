84b9d678c50911f100c0b7b7aea3dbc047b4e83e
Rubber-duck1234 <Kingsley.stockton@ucas-edu.net>
2026-01-23 14:42:17 -0700

    played with ints and floats

--- Notes/intergers and floats.py
+++ Notes/intergers and floats.py
@@ -1,7 +1,13 @@
-price = float(input("what is the price of your item: $"))
+"""price = float(input("what is the price of your item: $"))
 
 sales_tax = float(input("what is the sales tax: %"))/100
 
 total = round(price + (price * sales_tax), 2)
 
-print(total)
+print(total)"""
+
+apples = int(input("How many apples are there: "))
+
+friends = 8
+
+print("Each friend can have", apples//friends, "and I have", apples%friends, "left over")
