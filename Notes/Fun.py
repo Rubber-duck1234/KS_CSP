5f8478b2ef5dbd760a919e56bd532269074e37f7
Rubber-duck1234 <Kingsley.stockton@ucas-edu.net>
2026-01-26 13:57:56 -0700

    added fun.py

---
+++ Notes/Fun.py
@@ -0,0 +1,13 @@
+item1 = "Apples"
+item2 = "Bread"
+item3 = "milk"
+
+price1 = 1.50
+price2 = 2.00
+price3 = 2.75
+
+quantity1 = 3
+quantity2 = 1
+quantity3 = 2
+
+total = (price1 * quantity1) + (price2 * quantity2)
