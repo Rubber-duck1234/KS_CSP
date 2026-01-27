58debf08288a9d6ea5ec64648a6b2dffc2416748
Rubber-duck1234 <Kingsley.stockton@ucas-edu.net>
2026-01-27 14:40:04 -0700

    added strings

--- Notes/Strings.py
+++ Notes/Strings.py
@@ -1,5 +1,19 @@
-name = input("What is your name: ").strip().capitalize()
+"""#name = input("What is your name: ").strip().title()
 
 # To sanitize add . and then the thing you need.
 
-print("hello", name, "!")
+#print("hello", name + "!")
+
+sentance = "the quick brown fox jumps over the lazy dog"
+print(sentance)
+word = input("Tell me a word form the sentance: ")
+new_word = input("Give me a new word: ")
+start = sentance.find(word)
+lenglth = len(word)
+print(sentance. replace(word, new_word))
+
+#print(sentance[start:start + lenglth])"""
+
+num_one = int(input("Tell me a number: "))
+num_two = int(input("Tell me a number: "))
+print(num_one*num_two)
