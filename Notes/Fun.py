a6ae4ffc9505d731ff9769b1a133d31d819e95e8
Rubber-duck1234 <kingsley.stockton@ucas-edu.net>
2026-01-27 16:49:05 +0000

    added to fun

--- Notes/Fun.py
+++ Notes/Fun.py
@@ -2,12 +2,18 @@ item1 = "Apples"
 item2 = "Bread"
 item3 = "milk"
 
-price1 = 1.50
-price2 = 2.00
-price3 = 2.75
+price1 = .40
+price2 = 1.50
+price3 = 4
 
-quantity1 = 3
-quantity2 = 1
-quantity3 = 2
+quantity1 = float(input("How manny apples do you want, in numbers: "))
+quantity2 = float(input("How manny loaves of bread do you want, in numbers: "))
+quantity3 = float(input("How manny galons of milk do you want, in numbers: "))
 
-total = (price1 * quantity1) + (price2 * quantity2)
+total = (price1 * quantity1) + (price2 * quantity2) + (price3 * quantity3)
+print("Sales tax is 7% just so you know")
+
+round(total,2)
+
+tax = round(total/7,2)
+print("Your total is", tax + total)
