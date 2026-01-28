50db140ee9947bf85b32a730540a25d0838d3886
Rubber-duck1234 <Kingsley.stockton@ucas-edu.net>
2026-01-28 14:18:10 -0700

    finished silly sentances

---
+++ Work/silly_sentaces.py
@@ -0,0 +1,9 @@
+# KS, 7th , Silly Sentances
+
+verbing = input("Name me a verb ending in ing: ").strip().lower()
+place = input("Give me a contry on earth: ").strip().capitalize()
+day = input("Give me a day of the week: ").strip().capitalize()
+animal = input("Give me an animal that starts with a consonant: ").strip().lower()
+place2 = input("Give me another contry on earth: ").strip().capitalize()
+
+print("You went" + " " + verbing + " to" + " " + place + " on" + " " + day + " when sudenly a" + " " + animal + " fell from the sky and flung you all the way to" + " " + place2 + " !")
