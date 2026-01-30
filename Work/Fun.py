74f89cc14b68717ce400c616f00e5934b060d547
Rubber-duck1234 <Kingsley.stockton@ucas-edu.net>
2026-01-30 14:28:10 -0700

    added to fun

--- Work/Fun.py
+++ Work/Fun.py
@@ -4,4 +4,4 @@ if open == "Y":
     name = input("Unknown man- What is your name solder: ").strip().title()
     print(f"{name},hu okay, lets go")
 else:
-    print("You sit there is silence and then you feel the house blowup.")
+    print("You sit there is silence, and then you see the door to the house burst open and armed solders swarm in, you are taken away and will never be seen again.")
