fd2bd35c172ac69c2bb1e588cb9148b597c354ef
Rubber-duck1234 <Kingsley.stockton@ucas-edu.net>
2026-01-30 14:23:30 -0700

    moved comma

--- Work/Fun.py
+++ Work/Fun.py
@@ -2,6 +2,6 @@ print("You hear a knock on the door.")
 open = input("open? Y or N: ").strip().upper()
 if open == "Y":
     name = input("Unknown man- What is your name solder: ").strip().title()
-    print(f"{name} ,hu okay, lets go")
+    print(f"{name},hu okay, lets go")
 else:
     print("You sit there is silence and then you feel the house blowup.")
