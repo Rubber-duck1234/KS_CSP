192fa05b511da43a15fb4c46d800ea7c4313d2bd
Rubber-duck1234 <Kingsley.stockton@ucas-edu.net>
2026-01-30 14:29:12 -0700

    removed 0

--- Work/time.py
+++ Work/time.py
@@ -5,7 +5,7 @@ if time >= 8 and time <= 11:
     print("Good morning")
 elif time >= 12 and time <= 22:
     print("Good after noon")
-elif time >= 23 and time <= 24 or time >=0 and time <= 7:
+elif time >= 23 and time <= 24 or time >0 and time <= 7:
     print("Good evning and good night (also why are you up so late?)")
 elif time > 24:
     print("What do you think you are doing?")
