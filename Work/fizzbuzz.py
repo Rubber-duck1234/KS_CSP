67809e7b3d40b233229cfbfb5f6b69e451858037
Rubber-duck1234 <kingsley.stockton@ucas-edu.net>
2026-02-04 03:34:57 +0000

    h

--- Work/fizzbuzz.py
+++ Work/fizzbuzz.py
@@ -3,10 +3,10 @@ number = 1
 
 while number <= 50:
     print(number)
-    number += 1
     if number % 3 == 0:
         print("Fizz")
     elif number % 5 == 0:
         print("Buzz")
     elif number % 3 == 0 and number % 5 == 0:
         print("FizzBuzz")
+    number += 1
