8efbd3ffe8c3422236a99f95c3b78f7a61a7fce3
Rubber-duck1234 <kingsley.stockton@ucas-edu.net>
2026-02-05 16:52:18 +0000

    worked on update

---
+++ Work/update_calculator.py
@@ -0,0 +1,6 @@
+# KS, 7th, updated calculaator
+def all():
+    int(input("What is your monthly income: "))
+    int(input("What is your monthly rent: "))
+    int(input("What is you monthly utilities costs:"))
+    int(input("What is you monthly grocery cost: "))
