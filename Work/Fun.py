2994772342f52b9340f5f62e46be54f148b0fd49
Rubber-duck1234 <kingsley.stockton@ucas-edu.net>
2026-02-10 16:48:50 +0000

    added to fun

--- Work/Fun.py
+++ Work/Fun.py
@@ -1,8 +1,11 @@
 print("You hear a knock on the door.")
-open = input("open? Y or N: ").strip().upper()
-if open == "Y":
-    name = input("Unknown man- What is your name solder: ").strip().title()
-    print(f"{name},hu okay, lets go")
-else:
-    print("You sit there is silence, and then you see the door to the house burst open and armed solders swarm in, you are taken away and will never be seen again.")
-print("You see the car they laded you into, pull up to a base. They pull you out of the car and push you into the baracks.")
+while True:
+    open = input("open? Y or N: ").strip().upper()
+    if open == "Y":
+        name = input("Unknown man- What is your name solder: ").strip().title()
+        print(f"{name},hu okay, lets go")
+    else:
+        print("You sit there in silence, and then you see the door to the house burst open and armed solders swarm in, you are taken away and will never be seen again.")
+        break
+    print("You see the car they loaded you into, pull up to a base. They pull you out of the car and push you into the baracks............")
+    print("You wake up in your new home, a tank. It has been your home for the past four months.")
