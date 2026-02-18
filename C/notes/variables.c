6d8a7fd3fedecadc2ea145d4d83932e864fbbbec
Rubber-duck1234 <Kingsley.stockton@ucas-edu.net>
2026-02-18 14:42:09 -0700

    worked on notes

--- C/notes/variables.c
+++ C/notes/variables.c
@@ -2,6 +2,15 @@
 #include <stdio.h> // lets inputs and outputs
 
 int main(){
-    printf("It worked!");
+    // variable examples
+    int age = 9999;
+    float gpa = 3.98;
+    char grade = 'A'; // single letter is single quotes
+    char name[] = "Eric";
+
+    printf("It worked!\n");
+    printf("%d\n", age);
+    printf("Your GPA is: %f", gpa);
+    printf("You have a %c in computer science", grade);
     return 0; // requiered last line
 }
