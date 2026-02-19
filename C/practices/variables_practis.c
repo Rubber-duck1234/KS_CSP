6b81b280e551e43eea14fc4ede3278c654195552
Rubber-duck1234 <Kingsley.stockton@ucas-edu.net>
2026-02-19 14:34:20 -0700

    finished variables

--- C/practices/variables_practis.c
+++ C/practices/variables_practis.c
@@ -2,5 +2,16 @@
 #include <stdio.h>
 
 int main(){
-    
+    char name[] = ("Kingsley Stockton");
+    int luck_num = (8);
+    float dec_num = (99.9);
+    char breakfast[] = ("Eggs");
+    char color[] = ("Green");
+    char sch_name[] = ("UCAS");
+    char year[] = ("2026");
+    char eye[] = ("Green");
+    char age[] = ("15");
+    char subject[] = ("Biology");
+
+    printf("My name is %s, my lucky number is %d, my favorite decimal is %f, I had %s for breakfast, my favrite color is %s, I go to %s for school, it is the year %s, my favorite color is %s, I am %s, and my favorite subject is %s.", name, luck_num, dec_num, breakfast, color, sch_name, year, eye, age, subject);
 }
