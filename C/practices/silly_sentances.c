3984709904eaef2a6259992709447f826963f16e
Rubber-duck1234 <kingsley.stockton@ucas-edu.net>
2026-02-25 16:52:00 +0000

    worked

---
+++ C/practices/silly_sentances.c
@@ -0,0 +1,19 @@
+//KS, 7th, silly sentences
+#include <stdio.h>
+#include <strings.h>
+
+int main(){
+
+    char verb1[10];
+    char country1[20];// add a second word to concatanate to conutry
+    char thing1[20];
+
+    printf("Give me a verb: \n");
+    scanf("%s", &verb1);
+    printf("Give me a country: \n");
+    scanf("%s", &country1);
+    printf("Give me a random thing: \n");
+    scanf("%s", &thing1);
+
+    return 0;
+}
