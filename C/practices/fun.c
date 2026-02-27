3786073b41cc826a5ddda02fde13f1f3b19cb0c8
Rubber-duck1234 <kingsley.stockton@ucas-edu.net>
2026-02-27 17:49:34 +0000

    tryed to code fun

--- C/practices/fun.c
+++ C/practices/fun.c
@@ -1,9 +1,24 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdbool.h>
 
 int main(){
     
-    
+     char wepon[20];
+     int damg;
+
+    printf("Wellcome fighter!, choose your wepon: \n");
+    printf("Sword, bow, axe: \n");
+    scanf("%s", &wepon);
+    if (wepon == "sword"){
+     int damg = 20;
+    }else if (wepon == "bow"){
+     int dmag = 10;
+    }else if (wepon == "axe"){
+     int damg = 40;
+    }
+
+    printf("%d, \n", damg);
 
      return 0;
 }
