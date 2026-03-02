649fb396b30e113f1aa2d82ddef51bef77780e1e
Rubber-duck1234 <kingsley.stockton@ucas-edu.net>
2026-03-02 16:27:07 +0000

    did things

--- C/practices/fun.c
+++ C/practices/fun.c
@@ -4,21 +4,11 @@
 
 int main(){
     
-     char wepon[20];
-     int damg;
+     
 
-    printf("Wellcome fighter!, choose your wepon: \n");
-    printf("Sword, bow, axe: \n");
-    scanf("%s", &wepon);
-    if (wepon == "sword"){
-     int damg = 20;
-    }else if (wepon == "bow"){
-     int dmag = 10;
-    }else if (wepon == "axe"){
-     int damg = 40;
-    }
+    printf("Wellcome fighter! \n");
 
-    printf("%d, \n", damg);
+    
 
      return 0;
 }
