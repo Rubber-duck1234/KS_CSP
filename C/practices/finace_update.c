592dede3cbf96c26128c616ac8cd8d5c6428950e
Rubber-duck1234 <kingsley.stockton@ucas-edu.net>
2026-03-06 04:53:35 +0000

    FIX CODE

--- C/practices/finace_update.c
+++ C/practices/finace_update.c
@@ -1,19 +1,34 @@
+// KS, 7th, Updated calculator
 #include <stdio.h>
 
 
-char input(char* change){
+int input(char* change){
     printf("What is your monthly %s: $\n", change);
-    return scanf("%s", &change);
-    }
+    return scanf("%d", &change);
+}
+float pct(int thing, int income){
+    return (thing / income * 100);
+}
 
 int main(){
 
-    char income = input("income");
-    char rent = input("rent");
-    char utilities = input("utilities");
-    char groceries = input("groceries");
-    char transportation = input("transportation");
+    int income = input("income");
+    int rent = input("rent");
+    int utilities = input("utilities");
+    int groceries = input("groceries");
+    int transportation = input("transportation");
 
+    int rent_pct = pct(rent, income);
+    int utilities_pct = pct(utilities, income);
+    int groceries_pct = pct(groceries, income);
+    int transprtation_pct = pct(transportation, income);
+    int savings_pct = (income / 10);
+   
+    printf("Your monthly rent is %d$, that is %d percent of your income.\n", rent, rent_pct);
+    printf("Your monthly utilities is %d$, that is %d percent of your income.\n", utilities, utilities_pct);
+    printf("Your monthly groceries is %d$, that is %d percent of your income.\n", groceries, groceries_pct);
+    printf("Your monthly transportation is %d$, that is %d percent of your income.\n", transportation, transprtation_pct);
+    printf("Yoy should save %d, that is 10 percent of your income.\n", savings_pct);
 
     return 0;
 }
