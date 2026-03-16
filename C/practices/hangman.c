c943d036a89626d94fd36ceff5bbd5f4e74d392f
Rubber-duck1234 <Kingsley.stockton@ucas-edu.net>
2026-03-16 14:42:20 -0600

    worked on code

--- C/practices/hangman.c
+++ C/practices/hangman.c
@@ -1,7 +1,24 @@
 // KS, ,7th, Final
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+char let_guess[] = {};
+int loss = 0;
+int max = 8;
 
 int main(){
+    const char *random_word[] = {"onyx", "xylaphone", "glyph", "syzygy", "zuzwang", "house", "dermatoglyphics", "xebec", "bdellium", "grawlix"};
+    int num_words = sizeof (random_word)/ sizeof (random_word[0]);
+    srand(time(NULL));
+    int random = rand() % num_words;
+
+    const char *guess_word = random_word[random];
+
+
+printf("Instructions: To play hangman, you will be given a word you need to guess(by letter).Every time you guess a letter wrong, one part of a person will be built. If you end up guessing all the letters in the word, you win.However, if you guess too many wrong times and the person is built, you lose.");
+
+    
 
     return 0;
 }
