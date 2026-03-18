f91cab6bd56b7717bed4a3175709deba8b1af99e
Rubber-duck1234 <Kingsley.stockton@ucas-edu.net>
2026-03-18 14:41:51 -0600

    almost done with display

--- C/practices/hangman.c
+++ C/practices/hangman.c
@@ -2,10 +2,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-
-char let_guess[] = {};
+#include <string.h>
+int turn = 0;
+char let_guess[6] = {};
 int loss = 0;
 int max = 6;
+
 
 
 char building(char head,char body,char arm1,char arm2,char leg1,char leg2){
@@ -21,19 +23,25 @@ int main(){
     const char *guess_word = random_word[random];
 
 
-    printf("Instructions: To play hangman, you will be given a word you need to guess(by letter). Every time you guess a letter wrong, one part of a person will be built. If you end up guessing all the letters in the word, you win. However, if you guess too many wrong times and the person is built, you lose.\n");
-
-
+    printf("Instructions: To play hangman, you will be given a word you need to guess(by letter). Every time you guess a letter wrong, one part of a person will be built. If you end up guessing all the letters in the word, you win. However, if you guess wrong too many times you lose.\n");
 
     
     while(1 == 1){
-        char display[20];
+        char word_display[] = {0};
         char guess;
 
         printf("Guess a letter: ");
         scanf("%s", &guess);
 
+        char* check = strchr(guess_word, guess);
 
+        if(check != NULL){
+           let_guess[turn] = guess;
+        }else{
+            loss ++;
+            turn ++;
+        }
+        printf("%s", let_guess);
 
         if (loss == 0){
             printf("----------\n");
@@ -50,6 +58,8 @@ int main(){
             building('o','|','/','\\','/', ' ');
         }else if (loss == 6){
             building('o','|','/','\\','/', '\\');
+            printf("You lose.");
+            break;
         }
     }
 
