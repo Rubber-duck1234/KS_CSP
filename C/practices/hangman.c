373f5601e93303f5760112871b5f0319c22b942e
Rubber-duck1234 <kingsley.stockton@ucas-edu.net>
2026-03-21 00:15:11 +0000

    tryed

--- C/practices/hangman.c
+++ C/practices/hangman.c
@@ -3,55 +3,68 @@
 #include <stdlib.h>
 #include <time.h>
 #include <string.h>
-char let_guess[] = {"_" "_" "_" "_" "_" "_" "_" "_" "_" "_" "_" "_" "_" "_" "_"};
+char let_guess[100];
 int loss = 0;
-int max = 6;
 int turn = 0;
+ 
 
-
-char building(char head,char body,char arm1,char arm2,char leg1,char leg2){
+void building(char head,char body,char arm1,char arm2,char leg1,char leg2){
     printf("----------\n|        |\n|        %c\n|       %c%c%c\n|       %c%c\n|\n----------", head, arm1, body, arm2, leg1, leg2); // function for the structure of the hanman platform. KS
 }
 
 int main(){
     const char *random_word[] = {"onyx", "xylaphone", "glyph", "syzygy", "zuzwang", "house", "dermatoglyphics", "xebec", "bdellium", "grawlix"};// list of random words to use. IM
-    int num_words = sizeof (random_word)/ sizeof (random_word[0]);
+    int num_words = sizeof (random_word)/ sizeof (random_word[0]);// pulls random words from the list. KS
     srand(time(NULL)); 
     int random = rand() % num_words;
 
-    const char *guess_word = random_word[random];
+    const char *guess_word = random_word[rand() % num_words];
 
-    char guess_len = sizeof (guess_word)/ sizeof (guess_word[0]);// pulls random words from the list. KS
+    int guess_len = strlen (guess_word);
 
     printf("Instructions: To play hangman, you will be given a word you need to guess(by letter). Every time you guess a letter wrong, one part of a person will be built. If you end up guessing all the letters in the word, you win. However, if you guess wrong too many times you lose.\n");// instructions. IM
-
     
-    while(1 == 1){
-        char word_display[guess_len];
-        char guess;
+    char word_display[guess_len + 1];
+    for(int i = 0; i < guess_len; i++) word_display[i] = '_';
+    word_display[guess_len] = '\0';
+    
+
+
+    while(0 == 0){
         strcpy(word_display, guess_word);
 
+    
+        printf("\n%s\n", word_display);
+        printf("%s\n", let_guess);
+        char guess;
         printf("Guess a letter: \n");
-        scanf("%c", &guess);
-        let_guess[turn] = guess;
+        scanf(" %c", &guess);
 
         int correct = 0;
         for(int x = 0; x < guess_len; x++){// for loop to check if the guessed letter is in the word. KS
             if (guess_word[x] == guess){
                 word_display[x] = guess;
-                correct++;
+                correct = 1;
             }
         }
-        if(correct == 0){
+
+        let_guess[turn++] = guess;
+        let_guess[turn] = '\0';
+        if(!correct){
             loss++;// updates the losses.IM
         }
-        if (word_display == guess_word){
+        
+
+        printf("%s, these are the words you have guessed.\n", let_guess);
+        
+        if (strcmp(word_display, guess_word)==0){
             printf("You won!\n");
             break;
         }
+        
 
-        printf("%s, these are the words you have guessed.\n", let_guess);
-        turn++;
+
+
         if (loss == 0){
             printf("----------\n");
             printf("|\n|\n|\n|\n|\n|\n----------");
@@ -70,7 +83,7 @@ int main(){
             printf("You lose.\n");
             break;// IM
         }
-        
+
     }
 
     return 0;
